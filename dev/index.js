4edc279009212c34a5c7965ff999b8ecfeb04b25
charbelrami <charbel.f.rami@gmail.com>
2023-05-14 22:09:58 -0300

    rewrite

---
+++ dev/index.js
@@ -0,0 +1,145 @@
+import {
+  addChild,
+  addEventListener,
+  addGuardedChild,
+  addKeyedChildren,
+  createEffect,
+  createElement,
+  createGuardedEffect,
+  createRef,
+  createState,
+  createText,
+  getState,
+  onMount,
+  onUnmount,
+  setProperty,
+  setState,
+} from "../src";
+
+const count = createState(0);
+
+const counter = createElement(
+  "section",
+  addChild(createElement("h2", addChild(createText("Counter")))),
+  addChild(
+    createElement(
+      "button",
+      addEventListener("click", () => setState(count, getState(count) + 1)),
+      addChild(createText(() => getState(count), [count]))
+    )
+  )
+);
+
+const users = createState([]);
+const selectedUserId = createState();
+const posts = createState([]);
+
+createEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => setState(users, data));
+});
+
+createGuardedEffect(
+  () => {
+    fetch(
+      `https://jsonplaceholder.typicode.com/users/${getState(
+        selectedUserId
+      )}/posts`
+    )
+      .then((response) => response.json())
+      .then((data) => setState(posts, data));
+  },
+  () => getState(selectedUserId) !== undefined,
+  [selectedUserId]
+);
+
+const userPosts = createElement(
+  "section",
+  addChild(createElement("h2", addChild(createText("User Posts")))),
+  addChild(
+    createElement(
+      "label",
+      setProperty("htmlFor", "users"),
+      addChild(createText("Select a user"))
+    )
+  ),
+  addChild(
+    createElement(
+      "select",
+      setProperty("id", "users"),
+      setProperty("value", () => getState(selectedUserId), [selectedUserId]),
+      addEventListener("change", (e) =>
+        setState(selectedUserId, e.target.value)
+      ),
+      addKeyedChildren(
+        users,
+        (user) => [
+          user.id,
+          createElement(
+            "option",
+            setProperty("value", user.id),
+            addChild(createText(user.name))
+          ),
+        ],
+        [users]
+      )
+    )
+  ),
+  addGuardedChild(
+    () =>
+      createElement(
+        "ul",
+        addKeyedChildren(
+          posts,
+          (post) => [
+            post.id,
+            createElement("li", addChild(createText(post.title))),
+          ],
+          [posts]
+        )
+      ),
+    () => getState(posts).length > 0,
+    [posts]
+  )
+);
+
+const toggle = createState(false);
+
+const elementLifecycle = createElement(
+  "section",
+  addChild(createElement("h2", addChild(createText("Element Lifecycle")))),
+  addChild(
+    createElement(
+      "input",
+      setProperty("type", "checkbox"),
+      setProperty("id", "toggle"),
+      setProperty("checked", () => getState(toggle), [toggle]),
+      addEventListener("change", () => setState(toggle, !getState(toggle)))
+    )
+  ),
+  addChild(
+    createElement(
+      "label",
+      setProperty("htmlFor", "toggle"),
+      addChild(createText("Toggle element"))
+    )
+  ),
+  addGuardedChild(
+    () =>
+      createElement(
+        "p",
+        createRef((ref) => console.log(ref.deref())),
+        onMount(() => console.log("mounted")),
+        onUnmount(() => console.log("unmounted")),
+        addChild(createText("Hello, world!"))
+      ),
+    () => getState(toggle),
+    [toggle]
+  )
+);
+
+const root = document.getElementById("root");
+root.appendChild(counter);
+root.appendChild(userPosts);
+root.appendChild(elementLifecycle);
--- src/index.js
+++ src/index.js
@@ -1,119 +1,401 @@
-const reactionMap = new WeakMap();
-let reaction;
+const states = new WeakMap();
 
-export function createReaction(fn) {
-  reaction = fn;
-  reaction();
-  reaction = null;
-}
-
-export function createState(initialValue) {
-  let value = initialValue;
-  const getState = () => {
-    if (reaction) {
-      reactionMap.has(getState)
-        ? reactionMap.get(getState).add(reaction)
-        : reactionMap.set(getState, new Set([reaction]));
-    }
-    return value;
-  };
-  const setState = (newValue) => {
-    if (newValue !== value) {
-      value = newValue;
-      reactionMap.get(getState)?.forEach((reaction) => reaction());
-    }
-  };
-  return [getState, setState];
-}
-
-function render(element, children) {
-  children.forEach((child, i) => {
-    if (child === undefined) return;
-
-    if (["number", "string"].includes(typeof child)) {
-      const text = document.createTextNode(child);
-      return element.appendChild(text);
-    }
-
-    if (typeof child === "function") {
-      return createReaction(() => {
-        const result = child();
-
-        if (["number", "string"].includes(typeof result)) {
-          const text = document.createTextNode(result);
-          if (element.childNodes[i]?.nodeType === Node.TEXT_NODE)
-            return element.replaceChild(text, element.childNodes[i]);
-          return element.appendChild(text);
-        }
-
-        if (Array.isArray(result)) {
-          const { children } = element;
-          for (let i = 0; i < children.length; i++) {
-            const prevItem = children[i];
-            if (!result.find((node) => node.id === prevItem.id))
-              element.removeChild(prevItem);
-          }
-          for (let i = 0; i < result.length; i++) {
-            const prevItem = children[i];
-            const item = result[i];
-            if (prevItem) {
-              if (item.id !== prevItem.id) {
-                element.insertBefore(item, prevItem);
-                element.removeChild(prevItem);
-              }
-            } else {
-              element.appendChild(item);
-            }
-          }
-          return;
-        }
-
-        element.appendChild(result);
-      });
-    }
-
-    if (Array.isArray(child)) return render(element, child);
-
-    element.appendChild(child);
+export function createState(value) {
+  const key = Symbol();
+  states.set(key, {
+    value,
+    dependents: new Set(),
   });
+  return key;
 }
 
-const supportedSvgTags = ["svg", "path"];
-
-function setAttribute(element, key, value) {
-  if (key === "value") {
-    if (value === true) return (element.value = "");
-    return (element.value = value);
+function getStateByKey(key) {
+  const state = states.get(key);
+  if (!state) {
+    throw new Error(`No state found for key: ${String(key)}`);
   }
-  if (value || value === 0) {
-    if (value === true) return element.setAttribute(key, "");
-    return element.setAttribute(key, value);
-  }
-  return element.removeAttribute(key);
+  return state;
 }
 
-export function h(tag, props, ...children) {
-  if (typeof tag === "function") return tag({ ...props, children });
+export function getState(key) {
+  return getStateByKey(key).value;
+}
 
-  const element = supportedSvgTags.includes(tag)
-    ? document.createElementNS("http://www.w3.org/2000/svg", tag)
-    : document.createElement(tag);
+export function setState(key, value) {
+  const state = getStateByKey(key);
+  state.value = value;
+  state.dependents.forEach((dependent) => dependent());
+  return state.value;
+}
 
-  if (props)
-    Object.entries(props).forEach(([key, value]) => {
-      if (key === "ref") return value(element);
-      if (key.startsWith("on"))
-        return element.addEventListener(key.slice(2).toLowerCase(), value);
-      if (typeof value === "function")
-        return createReaction(() => setAttribute(element, key, value()));
-      return setAttribute(element, key, value);
+export function createEffect(callback, symbols = []) {
+  if (typeof callback !== "function") {
+    throw new Error(`'callback' must be a function.`);
+  }
+  if (!Array.isArray(symbols)) {
+    throw new Error(`'symbols' must be an array.`);
+  }
+  if (!symbols.length) {
+    callback();
+    return () => {};
+  }
+  const dependent = () => callback(...symbols.map(getState));
+  symbols.forEach((symbol) => {
+    const state = getStateByKey(symbol);
+    if (!state.dependents.has(dependent)) {
+      state.dependents.add(dependent);
+    }
+  });
+  return () => {
+    symbols.forEach((symbol) => {
+      const state = getStateByKey(symbol);
+      state.dependents.delete(dependent);
+    });
+  };
+}
+
+export function createGuardedEffect(callback, condition, symbols = []) {
+  if (typeof condition !== "function") {
+    throw new Error(`'condition' must be a function.`);
+  }
+  let prevState;
+  return createEffect(() => {
+    const currentState = symbols.map(getState);
+    if (condition(...currentState)) {
+      if (prevState !== currentState) {
+        callback(...currentState);
+      }
+    }
+    prevState = currentState;
+  }, symbols);
+}
+
+const dispatchChildrenUnmountEvents = (element) => {
+  element.addEventListener("unmount", () => {
+    element.childNodes.forEach((child) => {
+      dispatchUnmountEvent(child);
+    });
+  });
+  return element;
+};
+
+export const createElement = (type, ...modifiers) =>
+  modifiers.reduce(
+    (element, modifier) => modifier(element),
+    dispatchChildrenUnmountEvents(document.createElement(type))
+  );
+
+export const createSvgElement = (type, ...modifiers) =>
+  modifiers.reduce(
+    (element, modifier) => modifier(element),
+    document.createElementNS("http://www.w3.org/2000/svg", type)
+  );
+
+export const createText = (text, symbols) => {
+  if (symbols) {
+    if (typeof text !== "function") {
+      throw new Error(`'text' must be a function when 'symbols' are provided.`);
+    }
+    const node = document.createTextNode(text());
+
+    symbols.forEach((symbol) => {
+      const { dependents } = getStateByKey(symbol);
+      const dependent = () => {
+        const text_ = text();
+        if (node.nodeValue !== text_) {
+          node.nodeValue = text_;
+        }
+      };
+
+      dependents.add(dependent);
+
+      node.addEventListener("unmount", () => dependents.delete(dependent), {
+        once: true,
+      });
     });
 
-  render(element, children);
+    return node;
+  } else {
+    if (typeof text === "function") {
+      throw new Error("'symbols' must be provided when 'text' is a function.");
+    }
+    return document.createTextNode(text);
+  }
+};
+
+export const createRef = (callback) => (element) => {
+  const ref = new WeakRef(element);
+  callback(ref);
+  return element;
+};
+
+export const setProperty = (key, value, symbols) => (element) => {
+  if (symbols) {
+    if (typeof value !== "function") {
+      throw new Error(
+        `'value' must be a function when 'symbols' are provided.`
+      );
+    }
+    element[key] = value();
+    symbols.forEach((symbol) => {
+      const { dependents } = getStateByKey(symbol);
+      const dependent = () => {
+        const value_ = value();
+        if (element[key] !== value_) {
+          element[key] = value_;
+        }
+      };
+
+      dependents.add(dependent);
+
+      element.addEventListener("unmount", () => dependents.delete(dependent), {
+        once: true,
+      });
+    });
+  } else {
+    if (typeof value === "function") {
+      throw new Error(`'symbols' must be provided when 'value' is a function.`);
+    }
+    element[key] = value;
+  }
+  return element;
+};
+
+export const setAttribute = (key, value, symbols) => (element) => {
+  if (symbols) {
+    if (typeof value !== "function") {
+      throw new Error(
+        `'value' must be a function when 'symbols' are provided.`
+      );
+    }
+    element.setAttribute(key, value());
+    symbols.forEach((symbol) => {
+      const { dependents } = getStateByKey(symbol);
+      const dependent = () => {
+        const value_ = value();
+        if (element.getAttribute(key) !== value_) {
+          element.setAttribute(key, value_);
+        }
+      };
+
+      dependents.add(dependent);
+
+      element.addEventListener("unmount", () => dependents.delete(dependent), {
+        once: true,
+      });
+    });
+  } else {
+    if (typeof value === "function") {
+      throw new Error(`'symbols' must be provided when 'value' is a function.`);
+    }
+    element.setAttribute(key, value);
+  }
+  return element;
+};
+
+export const addEventListener = (type, listener) => (element) => {
+  element.addEventListener(type, listener);
+  element.addEventListener(
+    "unmount",
+    () => element.removeEventListener(type, listener),
+    { once: true }
+  );
+  return element;
+};
+
+function dispatchUnmountEvent(element) {
+  const event = new CustomEvent("unmount");
+  element.dispatchEvent(event);
+}
+
+export const onMount = (callback) => (element) => {
+  element.addEventListener("mount", callback, { once: true });
+  callback();
+  return element;
+};
+
+export const onUnmount = (callback) => (element) => {
+  element.addEventListener("unmount", callback, { once: true });
+  return element;
+};
+
+export const addChild = (child, symbols) => (element) => {
+  if (symbols) {
+    if (typeof child !== "function") {
+      throw new Error(
+        `'child' must be a function when 'symbols' are provided.`
+      );
+    }
+    let child_ = child();
+    element.appendChild(child_);
+
+    symbols.forEach((symbol) => {
+      const { dependents } = getStateByKey(symbol);
+      let prevChild = child_;
+
+      const dependent = () => {
+        const newChild = child();
+        if (!newChild.isEqualNode(prevChild)) {
+          element.replaceChild(newChild, prevChild);
+          prevChild = newChild;
+        }
+      };
+
+      dependents.add(dependent);
+
+      element.addEventListener(
+        "unmount",
+        () => {
+          dependents.delete(dependent);
+        },
+        { once: true }
+      );
+    });
+  } else {
+    if (typeof child === "function") {
+      throw new Error("'symbols' must be provided when 'child' is a function.");
+    }
+    element.appendChild(child);
+  }
+  return element;
+};
+
+export const addGuardedChild = (child, condition, symbols) => (element) => {
+  if (typeof child !== "function") {
+    throw new Error(`'child' must be a function.`);
+  }
+  if (typeof condition !== "function") {
+    throw new Error(`'condition' must be a function.`);
+  }
+  if (!Array.isArray(symbols)) {
+    throw new Error(`'symbols' must be an array.`);
+  }
+
+  let childElement;
+  let prevConditionResult;
+
+  const updateChild = () => {
+    const currentState = symbols.map(getState);
+    const currentConditionResult = condition(...currentState);
+
+    if (currentConditionResult !== prevConditionResult) {
+      if (currentConditionResult) {
+        if (!childElement) {
+          childElement = child();
+          element.appendChild(childElement);
+        } else {
+          const newChild = child();
+          if (!newChild.isEqualNode(childElement)) {
+            element.replaceChild(newChild, childElement);
+            childElement = newChild;
+          }
+        }
+      } else {
+        if (childElement) {
+          dispatchUnmountEvent(childElement);
+          element.removeChild(childElement);
+          childElement = null;
+        }
+      }
+    }
+    prevConditionResult = currentConditionResult;
+  };
+
+  symbols.forEach((symbol) => {
+    const { dependents } = getStateByKey(symbol);
+    dependents.add(updateChild);
+
+    element.addEventListener(
+      "unmount",
+      () => {
+        dependents.delete(updateChild);
+      },
+      { once: true }
+    );
+  });
+
+  updateChild();
 
   return element;
-}
+};
 
-export function Fragment({ children }) {
-  return children;
-}
+export const addKeyedChildren =
+  (childKey, createChildFunc, symbols) => (element) => {
+    if (typeof childKey !== "symbol") {
+      throw new Error(`'childKey' must be a symbol.`);
+    }
+    if (typeof createChildFunc !== "function") {
+      throw new Error(`'createChildFunc' must be a function.`);
+    }
+    if (!Array.isArray(symbols)) {
+      throw new Error(`'symbols' must be an array.`);
+    }
+
+    const childrenMap = new Map();
+    const startIndex = element.childNodes.length;
+    const cache = new WeakMap();
+
+    const memoizedCreateChildFunc = (arg) => {
+      if (cache.has(arg)) {
+        return cache.get(arg);
+      } else {
+        const result = createChildFunc(arg);
+        cache.set(arg, result);
+        return result;
+      }
+    };
+
+    const updateChildren = () => {
+      const children = getState(childKey).map(memoizedCreateChildFunc);
+      const newKeys = new Set(children.map(([key]) => key));
+
+      for (let [key, childElement] of childrenMap) {
+        if (!newKeys.has(key)) {
+          dispatchUnmountEvent(childElement);
+          element.removeChild(childElement);
+          childrenMap.delete(key);
+        }
+      }
+
+      let index = startIndex;
+      for (let [key, newChild] of children) {
+        let childElement = childrenMap.get(key);
+        if (!childElement) {
+          childElement = newChild;
+          element.insertBefore(childElement, element.childNodes[index]);
+          childrenMap.set(key, childElement);
+        } else {
+          if (!newChild.isEqualNode(childElement)) {
+            element.replaceChild(newChild, childElement);
+            childrenMap.set(key, newChild);
+          }
+          if (element.childNodes[index] !== childElement) {
+            element.insertBefore(childElement, element.childNodes[index]);
+          }
+        }
+        index++;
+      }
+    };
+
+    symbols.forEach((symbol) => {
+      const { dependents } = getStateByKey(symbol);
+      dependents.add(updateChildren);
+
+      element.addEventListener(
+        "unmount",
+        () => {
+          dependents.delete(updateChildren);
+        },
+        { once: true }
+      );
+    });
+
+    updateChildren();
+
+    return element;
+  };
+
+export const trace = (label) => (element) => {
+  console.log(label, element);
+  return element;
+};
